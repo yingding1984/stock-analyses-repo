d433740708b3e237d48e54f55fa8ab852d247cdd
yingding <yingding@paypal.com>
2013-09-05 15:42:26 +0800

    1

--- src/main/java/com/obv/core/util/HttpUnitUtil.java
+++ src/main/java/com/obv/core/util/HttpUnitUtil.java
@@ -18,59 +18,50 @@ import com.meterware.httpunit.WebTable;
  * 
  */
 public class HttpUnitUtil {
-	// structure of priceVolumeResults-- 1 Date 2 Open Price 5 Close Price 6
-	// Volumn of day
+    // structure of priceVolumeResults-- 1 Date 2 Open Price 5 Close Price 6
+    // Volumn of day
 
-	public static ArrayList<String> getStockTradeDetailFromDate(
-			String _stockID, String _year, String _month, String _date)
-			throws IOException, SAXException {
+    public static ArrayList<String> getStockTradeDetailFromDate(String _stockID, String _year, String _month,
+            String _date) throws IOException, SAXException {
 
-		String monthOfYahoo = getRealMonth(_month);
-		ArrayList<String> priceVolumeResults = new ArrayList<String>();
-		HttpUnitOptions.setScriptingEnabled(false);
-		WebConversation wc = new WebConversation();
-		WebResponse response = wc
-				.getResponse("http://finance.yahoo.com/q/hp?s=" + _stockID
-						+ ".SZ&a=" + monthOfYahoo + "&b=" + _date + "&c="
-						+ _year + "&d=08&e=4&f=" + _year + "&g=d");
+        String monthOfYahoo = getRealMonth(_month);
+        ArrayList<String> priceVolumeResults = new ArrayList<String>();
+        HttpUnitOptions.setScriptingEnabled(false);
+        WebConversation wc = new WebConversation();
+        WebResponse response;
+        if(_stockID.matches("^00") || _stockID.matches("^30")) {
+            response = wc.getResponse("http://finance.yahoo.com/q/hp?s=" + _stockID + ".SZ&a=" + monthOfYahoo + "&b="
+                    + _date + "&c=" + _year + "&d=08&e=4&f=" + _year + "&g=d");
+        } else {
+            response = wc.getResponse("http://finance.yahoo.com/q/hp?s=" + _stockID + ".SS&a=" + monthOfYahoo + "&b="
+                    + _date + "&c=" + _year + "&d=08&e=4&f=" + _year + "&g=d");
+        }
+        WebTable[] table = response.getTables();
+        if(table.length >= 4) {
+            String infoTable = table[4].getTableCell(1, 0).getText().replaceAll(",", "");
+            String[] strlines = infoTable.split("\n");
+            for(int i = 0; i < strlines.length; i++) {
+                if(strlines[i].contains(_year)) {
+                    priceVolumeResults.add(strlines[i]);
+                }
+            }
 
-		WebTable[] table = response.getTables();
+        }
 
-		String infoTable = table[4].getTableCell(1, 0).getText()
-				.replaceAll(",", "");
-		String[] strlines = infoTable.split("\n");
-		for (int i = 0; i < strlines.length; i++) {
-			System.out.println(strlines[i]);
-			if (strlines[i].contains(_year)) {
-				priceVolumeResults.add(strlines[i]);
-			}
-		}
+        return priceVolumeResults;
+    }
 
-		for (int i = 0; i < priceVolumeResults.size(); i++) {
+    private static String getRealMonth(String _month) {
+        String monthOfYahooStr = null;
+        Integer monthOfYahoo = Integer.parseInt(_month);
+        monthOfYahoo = monthOfYahoo - 1;
+        if(monthOfYahoo <= 10) {
+            monthOfYahooStr = "0" + monthOfYahoo;
+        } else {
+            monthOfYahooStr = monthOfYahoo.toString();
+        }
+        return monthOfYahooStr;
 
-			String recordLine = priceVolumeResults.get(i);
-			String[] recordColumns = recordLine.split("\\|");
-			System.out.println(i + " row data is : Date:'" + recordColumns[1]
-					+ "' Open:'" + recordColumns[2] + "' High:'"
-					+ recordColumns[3] + "' Low:'" + recordColumns[4]
-					+ "' Close:'" + recordColumns[5] + "' Volumn:'"
-					+ recordColumns[6] + "' ");
-		}
-
-		return priceVolumeResults;
-	}
-
-	private static String getRealMonth(String _month) {
-		String monthOfYahooStr = null;
-		Integer monthOfYahoo = Integer.parseInt(_month);
-		monthOfYahoo = monthOfYahoo - 1;
-		if (monthOfYahoo <= 10) {
-			monthOfYahooStr = "0" + monthOfYahoo;
-		} else {
-			monthOfYahooStr = monthOfYahoo.toString();
-		}
-		return monthOfYahooStr;
-
-	}
+    }
 
 }
--- src/main/java/com/obv/core/util/MySQLUtil.java
+++ src/main/java/com/obv/core/util/MySQLUtil.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.xml.sax.SAXException;
 
@@ -11,61 +13,69 @@ import com.obv.core.mysql.MySQLConnector;
 
 public class MySQLUtil {
 
-	static ResultSet rs;
-	private final static String CLEAN_DB = "drop table records";
-	private final static String GET_ALL_RECORDS = "drop table records";
-	private final static String CREATE_TABLE = "create  table  records(id  varchar(20),Date  varchar(20),open float,close float,volumn int,primary  key(id,Date))";
+    static ResultSet rs;
+    private final static String STOCK_LIST_FILE_PATH = "src/main/resources/stock-list";
+    private final static String CLEAN_DB = "truncate table records";
+    private final static String GET_ALL_RECORDS = "drop table records";
+    private final static String CREATE_TABLE = "create  table  records(id  varchar(20),Date  varchar(20),open float,close float,volumn int,primary  key(id,Date))";
 
-	public static void cleanDB() {
-		MySQLConnector.execute(CLEAN_DB);
-	}
+    public static void cleanDB() {
+        MySQLConnector.execute(CLEAN_DB);
+    }
 
-	public static void initialDB() {
-		MySQLConnector.execute(CREATE_TABLE);
-	}
+    public static void initialDB() {
+        MySQLConnector.execute(CREATE_TABLE);
+    }
 
-	public static ArrayList<String> getAllRecords() throws SQLException {
-		ArrayList<String> records = new ArrayList<String>();
-		rs = MySQLConnector.getResults(GET_ALL_RECORDS);
-		while (rs.next()) {
-			records.add(rs.toString());
-		}
-		return records;
-	}
+    public static ArrayList<String> getAllRecords() throws SQLException {
+        ArrayList<String> records = new ArrayList<String>();
+        rs = MySQLConnector.getResults(GET_ALL_RECORDS);
+        while(rs.next()) {
+            records.add(rs.toString());
+        }
+        return records;
+    }
 
-	public static void restoreStockDataToDB(String stockID, String year,
-			String month, String date) throws IOException, SAXException {
-		String tradeDate, openPrice, closePrice, volumn;
-		String restoreQuery;
-		ArrayList<String> priceVolumeResults = HttpUnitUtil
-				.getStockTradeDetailFromDate(stockID, year, month, date);
-		for (int i = 0; i < priceVolumeResults.size(); i++) {
-			String recordLine = priceVolumeResults.get(i);
-			String[] recordColumns = recordLine.split("\\|");
-			tradeDate = recordColumns[1];
-			openPrice = recordColumns[2];
-			closePrice = recordColumns[5];
-			volumn = recordColumns[6];
-			restoreQuery = "insert into records(id,Date,open,close,volumn) values ('"
-					+ stockID
-					+ "','"
-					+ tradeDate
-					+ "',"
-					+ openPrice
-					+ ","
-					+ closePrice + "," + volumn + ")";
-			MySQLConnector.execute(restoreQuery);
-		}
+    public static void restoreStockDataToDB(String stockID, String year, String month, String date) throws IOException,
+            SAXException {
+        String tradeDate, openPrice, closePrice, volumn;
+        String restoreQuery;
+        ArrayList<String> priceVolumeResults = HttpUnitUtil.getStockTradeDetailFromDate(stockID, year, month, date);
+        for(int i = 0; i < priceVolumeResults.size(); i++) {
+            String recordLine = priceVolumeResults.get(i);
+            String[] recordColumns = recordLine.split("\\|");
+            if(recordColumns.length >= 6) {
+                tradeDate = recordColumns[1];
+                openPrice = recordColumns[2];
+                closePrice = recordColumns[5];
+                volumn = recordColumns[6];
+                System.out.println("Storing trade record :" + stockID + ":" + tradeDate + " " + openPrice + " "
+                        + closePrice + " " + volumn);
+                restoreQuery = "insert into records(id,Date,open,close,volumn) values ('" + stockID + "','" + tradeDate
+                        + "'," + openPrice + "," + closePrice + "," + volumn + ")";
+                MySQLConnector.execute(restoreQuery);
+            }
+        }
 
-	}
-	
-	public static void restoreAllStockDataToDB(String year,
-			String month, String date){
-		String stockID;
-		//TO-DO get stock id list and execute storage
-	}
+    }
 
-	public static void main(String[] args) throws IOException, SAXException {
-		restoreStockDataToDB("002300","2013","07","29");
-	}
+    public static void restoreAllStockDataToDB(String year, String month, String date) throws Exception {
+        String stockID;
+        String stockList = FileLoader.loadFile(STOCK_LIST_FILE_PATH);
+        Pattern pattern = Pattern.compile("\\d{6}");
+        Matcher matcher = pattern.matcher(stockList);
+        while(matcher.find()) {
+            stockID = matcher.group();
+            System.out.println("storing " + stockID);
+            restoreStockDataToDB(stockID, year, month, date);
+            Thread.sleep(5000);
+        }
+        // TO-DO get stock id list and execute storage
+    }
+
+    public static void main(String[] args) throws Exception {
+//        cleanDB();
+         restoreStockDataToDB("600318", "2013", "07", "29");
+//        restoreAllStockDataToDB("2013", "08", "01");
+    }
 }
