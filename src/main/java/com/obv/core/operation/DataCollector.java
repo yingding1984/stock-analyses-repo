08af172d91efce385c12ea811fea21be85da8f78
tangbao <tangbao@tangbao-PC>
2013-09-06 22:58:58 +0800

    1

--- src/main/java/com/obv/core/operation/DataCollector.java
+++ src/main/java/com/obv/core/operation/DataCollector.java
@@ -12,11 +12,10 @@ import com.obv.core.mysql.util.MySQLUtil;
  * 
  */
 public class DataCollector {
-	public static void main(String[] args) throws IOException, SAXException,
-			SQLException {
+	public static void main(String[] args) throws Exception {
 		MySQLUtil.collectStockDataToDB("600319", "2013", "07", "29", "2013",
 				"09", "05");
-		// MySQLUtil.restoreAllStockDataToDB("2013", "08",
-		// "01","2013","09","05");
+//		MySQLUtil.collectAllStockDataToDB("2013", "08", "01", "2013", "09",
+//				"05");
 	}
 }
