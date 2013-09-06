5ce607d128fae436869ddeb7b447bbcc1024e135
yingding <yingding@paypal.com>
2013-09-06 17:10:07 +0800

    1

--- src/main/java/com/obv/core/util/PDFLoader.java
+++ src/main/java/com/obv/core/util/PDFLoader.java
@@ -11,17 +11,30 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
  * 
  */
 public class PDFLoader {
+    private final static String STOCK_LIST_FILE_PATH = "src/main/resources/JunXu.pdf";
 
-    public static void readPDF(String path) throws Exception {
+    public static String readPDF(String path) throws Exception {
         try {
 
             PdfReader reader = new PdfReader(path);
             int n = reader.getNumberOfPages();
-            System.out.println("page number = " + n);
-            String str = PdfTextExtractor.getTextFromPage(reader, n); // Extracting
-            System.out.println(str);
+            String content = new String();
+            for(int i = 1; i < n; i++) {
+                System.out.println("**********************************PAGE NUMBER " + i
+                        + "*************************************************************************************");
+                String contentLine = PdfTextExtractor.getTextFromPage(reader, i);
+                System.out.println(contentLine);
+                content += contentLine;
+            }
+            return content;
         } catch (Exception e) {
             System.out.println(e);
+            return null;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // System.out.println(readPDF(STOCK_LIST_FILE_PATH));
+        readPDF(STOCK_LIST_FILE_PATH);
     }
 }
